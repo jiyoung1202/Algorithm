cb2f1c973d10557117d6496b5922005bc3661458
jiyoung lee <bestjy1202@nate.com>
2019-04-18 00:33:39 +0900

    소수만들기

---
+++ src/summercoding2017_primenumber/Main.java
@@ -0,0 +1,13 @@
+package summercoding2017_primenumber;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int nums [] ={1,2,7,6,4};
+
+        solution1 solution1 = new solution1();
+        System.out.println(solution1.solution(nums));
+
+    }
+}
