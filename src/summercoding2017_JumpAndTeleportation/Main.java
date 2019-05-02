76b9260c2b731df86ab97790f0fa38f30d621e84
jiyoung lee <bestjy1202@nate.com>
2019-05-02 20:21:35 +0900

    solve jumpAndTeleportation

---
+++ src/summercoding2017_JumpAndTeleportation/Main.java
@@ -0,0 +1,9 @@
+package summercoding2017_JumpAndTeleportation;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=5000;
+        solution1 solution1 = new solution1();
+        System.out.println(solution1.solution(n));
+    }
+}
