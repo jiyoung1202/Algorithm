38a80b789e50e0df7cc53107bfc4fd1db67d2a8b
jiyoung lee <bestjy1202@nate.com>
2019-05-03 00:51:31 +0900

    solve PrimeNumber

--- src/summercoding2017_primenumber/Main.java
+++ src/summercoding2017_primenumber/Main.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int nums [] ={1,2,7,6,4};
-
         solution1 solution1 = new solution1();
         System.out.println(solution1.solution(nums));
 
--- src/summercoding2017_primenumber/solution1.java
+++ src/summercoding2017_primenumber/solution1.java
@@ -2,41 +2,37 @@ package summercoding2017_primenumber;
 
 public class solution1 {
     public int solution(int[] nums) {
-        int answer = -1;
+        int answer = 0;
         boolean isPrime=true;
         int count = 0;
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 for(int k=j+1; k<nums.length; k++){
                     int number = nums[i]+nums[j]+nums[k];
-//                    System.out.println(number);
+                    System.out.println(number);
                     int l=2;
                     while(l<number){
-//                    for(int l=2; l<number; l++){
-                        if(number % l ==0) {
-                            System.out.println(l+"로 나눕니다.");
+                        if(number % l ==0 && number!=2) {
+//                            System.out.println(l+"로 나눕니다.");
                             isPrime = false;
-                            System.out.println(number + "는 소수가 아닙니다.");
+//                            System.out.println(number + "는 소수가 아닙니다.");
+//                            l++;
                             break;
+                        }else{
+//                            System.out.println(number+ "는 소수 입니다.");
+                            isPrime=true;
                         }
                         l++;
-//                        else{
-//                            System.out.println(number+ "는 소수 입니다.");
-//                            count++;
-//                            System.out.println(count);
-//                            answer = count;
-//                            break;
-//                        }
-                    }
-                    if(isPrime==true){
+//
+
+                    }  if(isPrime==true){
                         answer++;
-                        System.out.println("---------");
+//                        System.out.println("---------");
                     }
 
                 }
             }
         }
-
         return answer;
     }
 }
